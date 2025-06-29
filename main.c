5fe349d2a296575d7db018309781d685bcda1045
Robert Chapman <rc@interbaun.com>
2025-06-29 01:54:16 -0600

    added test frame and factored bit sequencer

--- main.c
+++ main.c
@@ -1,18 +1,26 @@
 // Tea testing
 #include "tea.c"
 
-bool bit(Byte * p, Byte bit) { return 1 & p[bit / 8] >> (7 - bit % 8); }
+static Byte bit_sync[6] = {0xEB, 0x90, 0xB4, 0x33, 0xAA, 0xAA};
+static Byte bit_seq[1000];
 
-void spit_bits_sync() {
-    Byte bit_sync[6] = {0xEB, 0x90, 0xB4, 0x33, 0xAA, 0xAA};
-    print("\nBit Sync in bits: ");
-    bool first = bit(bit_sync, 0);
+static Byte test_frame[] = {
+    0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, // preamble
+    0xEB, 0x90, 0xB4, 0x33, 0xAA, 0xAA, // bit sync
+    0x35, 0x2E, 0xF8, 0x53, // frame sync
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 250, 251, 252, 253, 254, 255,
+};
+
+static Byte test_frame_bits[sizeof(test_frame) * 8];
+
+static bool bit(Byte * p, Short bit) { return 1 & p[bit / 8] >> (7 - bit % 8); }
+
+static void frame_bits(Byte * frame, Short n, Byte * bit_seq) {
+    bool first = bit(frame, 0);
     Byte seq = 0;
-    Byte bit_seq[1000] = {0};
     Byte * bp = bit_seq;
-    for (Byte i = 0; i < sizeof(bit_sync) * 8; i++) {
-        bool b = bit(bit_sync, i);
-        printDec0(b);
+    for (Short i = 0; i < n * 8; i++) {
+        bool b = bit(frame, i);
         if (b == first)
             seq++;
         else {
@@ -22,14 +30,24 @@ void spit_bits_sync() {
         }
     }
     *bp++ = seq;
-    printCr();
-    bp = bit_seq;
-    while (*bp)
-        printDec0(*bp++);
 }
 
-void init_app() {
-    later(spit_bits_sync);
+static void test_bits_sync() {
+    frame_bits(bit_sync, sizeof(bit_sync), bit_seq);
+    printCr();
+    Byte * bp = bit_seq;
+    while (*bp)
+        printDec0(*bp++);
+
+    frame_bits(test_frame, sizeof(test_frame), test_frame_bits);
+    printCr();
+    bp = test_frame_bits;
+    while (*bp)
+        printDec0(*bp++);
+    }
+
+static void init_app() {
+    later(test_bits_sync);
 }
 
 int main() {
