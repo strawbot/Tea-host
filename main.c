4698eb69c43c79b35510a3b0123a6cdcae6e3ec4
Robert Chapman <rc@interbaun.com>
2025-06-29 13:22:41 -0600

    simple frame matching

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 // Tea testing
 #include "tea.c"
+#include <string.h>
 
 static Byte bit_sync[6] = {0xEB, 0x90, 0xB4, 0x33, 0xAA, 0xAA};
-static Byte bit_seq[1000];
+static Byte bit_sync_bits[1000];
 
 static Byte test_frame[] = {
     0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, // preamble
@@ -15,10 +16,10 @@ static Byte test_frame_bits[sizeof(test_frame) * 8];
 
 static bool bit(Byte * p, Short bit) { return 1 & p[bit / 8] >> (7 - bit % 8); }
 
-static void frame_bits(Byte * frame, Short n, Byte * bit_seq) {
+static void frame_bits(Byte * frame, Short n, Byte * bit_sync_bits) {
     bool first = bit(frame, 0);
     Byte seq = 0;
-    Byte * bp = bit_seq;
+    Byte * bp = bit_sync_bits;
     for (Short i = 0; i < n * 8; i++) {
         bool b = bit(frame, i);
         if (b == first)
@@ -32,22 +33,44 @@ static void frame_bits(Byte * frame, Short n, Byte * bit_seq) {
     *bp++ = seq;
 }
 
-static void test_bits_sync() {
-    frame_bits(bit_sync, sizeof(bit_sync), bit_seq);
-    printCr();
-    Byte * bp = bit_seq;
-    while (*bp)
-        printDec0(*bp++);
-
+static void build_bit_seqs() {
+    frame_bits(bit_sync, sizeof(bit_sync), bit_sync_bits);
     frame_bits(test_frame, sizeof(test_frame), test_frame_bits);
-    printCr();
-    bp = test_frame_bits;
-    while (*bp)
-        printDec0(*bp++);
+}
+
+// last bit might be 1 or more so it will match if equal to or > 
+// in the case the last bit is 1, it will always match except for end of string
+static Short match_bit_seqs(Byte * sync, Byte * frame) {
+    Byte * mp = sync;
+    Byte * sp = frame;
+    while (*sp && *mp)
+        if (*sp++ != *mp++) {
+            if (*mp == 0 && mp[-1] <= sp[-1]) // check last match
+                break;
+            sp -= mp - sync - 1; // back to start of search plus 1
+            mp = sync;
+        }
+    if (*mp == 0) {
+        sp -= strlen((char *)sync);
+        return sp - frame;
     }
+    return strlen((char*)test_frame_bits);
+}
+
+static void print_results() {
+    Short index = match_bit_seqs(bit_sync_bits, test_frame_bits);
+    Short tlen = strlen((char*)test_frame_bits);
+    if (index == tlen)
+        print("\nNo match");
+    else {
+        print("\nBit match at: "), printDec(index);
+        print(" out of: "), printDec(tlen);
+    }
+}
 
 static void init_app() {
-    later(test_bits_sync);
+    later(build_bit_seqs);
+    later(print_results);
 }
 
 int main() {
