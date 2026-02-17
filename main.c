bac86bdf62d105fcc36a78303b6d14f58f2f1fc0
Robert Chapman <rc@interbaun.com>
2026-02-17 03:30:27 -0700

    clean up and factoring

--- main.c
+++ main.c
@@ -6,23 +6,24 @@
 #include <string.h>
 #include <stdlib.h>
 
-void init_zc();
+#define view 80
+
 void init_decoder();
 void test_mls();
 void test_rs012();
 void test_convo();
 void init_zc();
 void serve_tea();
-Long frame_bits(Byte * frame, Short n, Byte * bits);
+Long bit_sequenced(Byte * frame, Short n, Byte * bits);
 Long find_bit_sync(Byte * bits);
 Long find_frame_sync(Byte * bits);
 Short bits_to_bytes(Byte * bits, Byte * bytes, Short len);
-Short puncbits_to_bytes(Byte * bits, Byte * bytes, Short nbits, Byte mode);
 int deconvolve_buffer(Byte * frame, Byte * decoded, Short length);
 int decode_rs(Byte * data, Long size);
-Short count_bits(Byte * bits, Short nbits);
 Byte * trim_bits(Byte * seq, Short bits);
-Short match_bit_seqs(Byte * sync, Byte * frame);
+
+// Short count_bits(Byte * bits, Short nbits);
+// Short match_bit_seqs(Byte * sync, Byte * frame);
 
 static Byte alpdu1[] = {0x00, 0x00, 0x10, 0x0B, 0x0B, 0xB8, 0x44, 0x5A, 0xEC,
                         0x01, 0x06, 0x08, 0x11, 0x84, 0xC9, 0x11, 0x04};
@@ -107,7 +108,6 @@ static Byte alpdu3[] = {0x04, 0x00, 0x10, 0xD5, 0x03, 0xE8, 0x10, 0x02, 0x80, 0x
 
 extern Byte bit_sync_bits[BSYNC_SIZE * 8];
 extern Byte frame_sync_bits[3][FSYNC_SIZE * 8];
-#define view 80
 
 static Byte fec = 0, pdu = 0;
 static Byte * pdus[3] = {alpdu1, alpdu2, alpdu3};
@@ -153,105 +153,48 @@ void fec2_bits(Long n) { // n bytes is how many coded bits with tail and punctur
     print("bytes convolved with tail becomes "), printDec(bits), print("bits  ");
 }
 
-void continue_test() {
-    AirFrame * afe = get_airframe();
-    Byte bits[8 * afe->nbytes];
-    memset(bits, 0, sizeof(bits));
-    afe->bits = bits;
-    Short fbits = frame_bits(afe->bytes, afe->nbits, afe->bits);
-    // print(" frame_bits: "), printDec(fbits); // nbits is number of sequences
-    
-    // print("\n Encoded: "), printDec(afe->nbytes), print("bytes  ");//, hbytes(afe->bytes, min(30, afe->nbytes));
-    // print("\n  As bit sequences: "), printDec(afe->nbits), print("bits  "), hbytes(afe->bits, min(30, afe->nbits));
-    // print("\nDecoding: ");
-    // Short b;
-    // printDec((b = count_bits(afe->bits, fbits))), print("bits  ");
-    // printDec(b/8), print("bytes");
-
-    // print("\n Find bit sync:");
-    AirFrame afd = {.bits = afe->bits, .nbits = fbits};
-    Long offset = find_bit_sync(afd.bits - 1);
-    // print(" offset = "), printDec(offset);
-    if (offset >= afd.nbits) {
+void calc_seg1(AirFrame * afd) {
+    Long offset = find_bit_sync(afd->bits);
+    if (offset >= afd->nbits) {
         print("  No bit sync match ");
         return;
     }
 
-    afd.bits = trim_bits(afd.bits, offset);
-    afd.nbits -= offset; // recalculate length after removing syncs
+    afd->bits = trim_bits(afd->bits, offset);
+    afd->nbits -= offset; // recalculate length after removing syncs
 
-    Byte bytes[afd.nbits+1];
-    afd.bytes = bytes;
-    afd.nbytes = bits_to_bytes(afd.bits, afd.bytes, afd.nbits);
+    afd->nbytes = bits_to_bytes(afd->bits, afd->bytes, afd->nbits);
 
-    // print("\n Bits to Bytes: "); 
-    // printDec(afd.nbytes), print("bytes "); hbytes(afd.bytes, min(40, afd.nbytes));
- 
-    Byte fecmode = find_frame_sync(afd.bytes + BSYNC_SIZE);
-    // print("\n FEC mode: ");
-    // printDec(fecmode);
-    if (fecmode > 2) { print("\n Cannot decode further! Bad FEC mode!"); return; }
+    afd->fec = find_frame_sync(afd->bytes + BSYNC_SIZE);
+    if (afd->fec > 2) { print("\n Cannot decode further! Bad FEC mode!"); return; }
 
+    afd->nrs = afd->fec == 0 ? FEC0_NROOTS : FEC12_NROOTS; // nroots
+    afd->seg1 = afd->bytes + SYNC_SIZE;
+    afd->nseg1 = afd->fec == 0 ? SEG10_SIZE : afd->fec == 1 ? SEG11_SIZE : SEG12_SIZE;
+    afd->nseg2 = afd->nbytes - SYNC_SIZE > afd->nseg1 ? afd->nbytes - SYNC_SIZE - afd->nseg1 : 0;
+}
 
-    // print("\n Split into segments: ");
-    afd.nrs = fecmode == 0 ? FEC0_NROOTS : FEC12_NROOTS; // nroots
-    afd.seg1 = afd.bytes + SYNC_SIZE;
-    afd.nseg1 = fecmode == 0 ? SEG10_SIZE : fecmode == 1 ? SEG11_SIZE : SEG12_SIZE;
-    afd.nseg2 = afd.nbytes - SYNC_SIZE > afd.nseg1 ? afd.nbytes - SYNC_SIZE - afd.nseg1 : 0;
-    Byte seg2[afd.nseg2 ? afd.nseg2 : 1];
-    afd.seg2 = afd.nseg2 ? afd.seg1 + afd.nseg1 : NULL;
-    // printDec((afd.seg1 != NULL) + (afd.seg2 != NULL));
-    // printDec(afd.nseg1 + afd.nseg2), print("bytes  ");
-    // switch(fecmode) {
-    // case 0: if (afd.nseg1 != SEG10_SIZE) print(" Segment Wrong size"); printDec(SEG10_SIZE),printDec(afd.nseg1); break;
-    // case 1: if (afd.nseg1 != SEG11_SIZE) print(" Segment Wrong size"); printDec(SEG11_SIZE),printDec(afd.nseg1); break;
-    // case 2: if (afd.nseg1 != SEG12_SIZE) print(" Segment Wrong size"); printDec(SEG12_SIZE),printDec(afd.nseg1); break;
-    // }
-
-    // print("\n Deconvolve segment 1: "); // nseg1 length is for data only
-    Byte seg1[afd.nseg1];
-    // printDec(afd.nseg1), print("bytes  ");
-// print("\n predeconvolve: "),printDec(afd.nseg1),hbytes(afd.seg1, afd.nseg1), print("  ");
-    int n = deconvolve_buffer(afd.seg1, seg1, afd.nseg1*8) - afd.nrs;
-    if (n < 0) { print("\n Faild deconvolving"); return; }
-    afd.nseg1 = n;
-    // print("  NSEG1 = "), printDec(afd.nseg1);
-    // print("\n  Decode errors in block 1: ");
-// hbytes(seg1, n + afd.nrs), print("  ");
-    // printDec()
-    decode_rs(seg1, afd.nseg1);
-
-    // print("\n  Descramble block 1: ");
-    unscramble_blk(seg1, afd.nseg1);
-    // hbytes(seg1, min(40,afd.nseg1));
-
-    // Assemble block 1
-    Short len = seg1[0] << 8 | seg1[1];
-    if (len > 1023) { print("\n Length is too big, aborting decoding"); return; }
-    Byte airpdu[len];
-    Short nseg1 = min(len, afd.nseg1 - 2);
+void create_airpdu(AirFrame * afd, Byte * seg1, Byte * airpdu) {
+    Short nseg1 = min(afd->len, afd->nseg1 - 2);
     memcpy(airpdu, seg1 + 2, nseg1);
 
-    if (afd.nseg2) {
-        // print("\n Deconvolve segment 2: ");
-        // printDec(afd.nseg2), print("bytes  ");
+    if (afd->nseg2) {
+        Byte seg2[afd->nseg2];
         bool valid = true;
-        switch(fecmode) {
-        case 0:  valid = afd.nseg2 >= MIN_SEG10_SIZE && afd.nseg2 < 10000; break;
-        case 1:  valid = afd.nseg2 >= MIN_SEG11_SIZE && afd.nseg2 < 10000; break;
-        case 2:  valid = afd.nseg2 >= MIN_SEG12_SIZE && afd.nseg2 < 10000; break;
+        switch(afd->fec) {
+        case 0:  valid = afd->nseg2 >= MIN_SEG10_SIZE && afd->nseg2 < 10000; break;
+        case 1:  valid = afd->nseg2 >= MIN_SEG11_SIZE && afd->nseg2 < 10000; break;
+        case 2:  valid = afd->nseg2 >= MIN_SEG12_SIZE && afd->nseg2 < 10000; break;
         }
         if (valid) {
-            // print("\n  Decode errors in blocks: ");
-            n = deconvolve_buffer(afd.seg2, seg2, afd.nseg2*8);
+            int n = deconvolve_buffer(afd->seg2, seg2, afd->nseg2*8);
             if (n < 0) { print("\n Faild deconvolving"); return; }
-            afd.nseg2 = n;
-            Byte blockm = fecmode == 0 ? BLOCKm0 : fecmode == 1 ? BLOCKm1 : BLOCKm2;
-            Byte rsblock = blockm + afd.nrs;
-            for (Short s = 0, d = 0; s < afd.nseg2; s += rsblock, d += blockm) {
-                int n = min(rsblock, afd.nseg2 - s) - afd.nrs;
+            afd->nseg2 = n;
+            Byte blockm = afd->fec == 0 ? BLOCKm0 : afd->fec == 1 ? BLOCKm1 : BLOCKm2;
+            Byte rsblock = blockm + afd->nrs;
+            for (Short s = 0, d = 0; s < afd->nseg2; s += rsblock, d += blockm) {
+                int n = min(rsblock, afd->nseg2 - s) - afd->nrs;
                 if (n > 0) {
-                    // printDec()
                         decode_rs(seg2 + s, n);
                     unscramble_blk(seg2 + s, n);
                     memcpy(airpdu + nseg1 + d, seg2 + s, n);
@@ -261,12 +204,30 @@ void continue_test() {
         } else
             print(" Segment Wrong size! ");
     }
+}
 
-    // print("\n Assemble blocks: ");
-    // print("\n airpdu:  ");
-    // printDec(len), print("bytes  "), hbytes(airpdu, min(40, len));
-    if (len == test_size && memcmp(airpdu, test_pdu, test_size) == 0)
-        success++ /*, print("  Matched ")*/;
+void continue_test() {
+    AirFrame * afe = get_airframe();
+    Byte bits[8 * afe->nbytes];
+    memset(bits, 0, sizeof(bits));
+    AirFrame afd = {.bits = bits, .nbits = bit_sequenced(afe->bytes, afe->nbits, bits)};
+    Byte bytes[afd.nbits+1];
+    afd.bytes = bytes;
+    calc_seg1(&afd);
+    Byte seg1[afd.nseg1];
+    afd.seg2 = afd.nseg2 ? afd.seg1 + afd.nseg1 : NULL;
+    afd.nseg1 = deconvolve_buffer(afd.seg1, seg1, afd.nseg1*8) - afd.nrs;
+    if (afd.nseg1 < 0) { print("\n Faild deconvolving"); return; }
+    decode_rs(seg1, afd.nseg1);
+    unscramble_blk(seg1, afd.nseg1);
+
+    // Assemble block 1
+    afd.len = seg1[0] << 8 | seg1[1];
+    if (afd.len > 1023) { print("\n Length is too big, aborting decoding"); return; }
+    Byte airpdu[afd.len];
+    create_airpdu(&afd, seg1, airpdu);
+    if (afd.len == test_size && memcmp(airpdu, test_pdu, test_size) == 0)
+        success++;
     else {
         print("  Different! ");
         for (Short i = 0; i < test_size; i++)
@@ -275,8 +236,6 @@ void continue_test() {
                 break;
             }
     }
-    // printCr();
-
     finish_test();
 }
 
